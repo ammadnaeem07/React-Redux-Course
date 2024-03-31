386a399c39c24c1e8f467c9f00d3b41a52d91f96
Ammad Naeem <ammad.naeem07@gmail.com>
2024-04-01 01:42:11 +0500

    Use context API example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import {
   ProjectIndex,
   ProjectDetail,
   CounterComponent,
-} from "./components/Project";
-import { ProjectFrom } from "./contactproject/contact";
+} from './components/Project';
+import { ProjectFrom } from './contactproject/contact';
+import HOCApp from './components/MyHocComponent';
+import { DefaultHooks } from './components/DefaultHooks';
+import { UserComponent } from './ContextAPIHook/UserComponent';
+import { UserProvider } from './ContextAPIHook/UserContext';
+import { DemoComponent } from './components/DemoComponent';
 
 function App() {
   return (
     <div className="container">
       {/* <ProjectIndex/> */}
-      <ProjectFrom />
+      {/* <ProjectFrom /> */}
       {/* <CounterComponent/> */}
+      {/* <HOCApp/> */}
+      {/* <DefaultHooks/> */}
+      <UserProvider>
+        <UserComponent />
+        <DemoComponent />
+      </UserProvider>
     </div>
   );
 }
